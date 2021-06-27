77b9f0cc6702551f0837796d41b9695a2f4b4be9
Kirill Kravchenko <clearworkercorp@gmail.com>
2021-06-28 05:59:28 +0700

    feat: add throttler (rate-limiter)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { config } from './config/config'
+import { throttlerConfig } from './config/throttler.config'
 import { typeormConfig } from './config/typeorm.config'
+import { StoresModule } from './stores/stores.module'
 
 @Module({
     imports: [
         ConfigModule.forRoot(config),
         TypeOrmModule.forRootAsync(typeormConfig),
+        ThrottlerModule.forRootAsync(throttlerConfig),
+        StoresModule,
     ],
 })
 export class AppModule {}
