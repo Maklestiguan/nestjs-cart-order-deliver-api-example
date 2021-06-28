b70ce5cc4edb9dd4a34beaed80af5f82eae9a0df
Kirill Kravchenko <clearworkercorp@gmail.com>
2021-06-28 07:33:44 +0700

    chore: add global exceptions filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { APP_FILTER } from '@nestjs/core'
 import { ThrottlerModule } from '@nestjs/throttler'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { config } from './config/config'
 import { throttlerConfig } from './config/throttler.config'
 import { typeormConfig } from './config/typeorm.config'
+import { AllExceptionsFilter } from './shared/exception-filters/all-exceptions.filter'
 import { StoresModule } from './stores/stores.module'
 
 @Module({
@@ -13,6 +15,12 @@ import { StoresModule } from './stores/stores.module'
         TypeOrmModule.forRootAsync(typeormConfig),
         ThrottlerModule.forRootAsync(throttlerConfig),
         StoresModule,
+    ],
+    providers: [
+        {
+            provide: APP_FILTER,
+            useClass: AllExceptionsFilter,
+        },
     ],
 })
 export class AppModule {}
